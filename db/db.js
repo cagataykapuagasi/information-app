9d9a09de6843e9cf81c37b59fab468ee2a39d102
Cagatay <cagatay.kapuagasi@gmail.com>
2021-03-25 00:18:21 +0300

    build v8

--- db/db.js
+++ db/db.js
@@ -18,14 +18,14 @@ const sequelize = new Sequelize(
   }
 );
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log("Connection has been established successfully.");
+//   })
+//   .catch((err) => {
+//     console.error("Unable to connect to the database:", err);
+//   });
 
 const Model = Sequelize.Model;
 class User extends Model {}
